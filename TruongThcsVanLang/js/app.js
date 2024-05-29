5abe052f7bbe1ef9d1499bd7a3ee87140c5996cc
Ones <minhquang2002nt@gmail.com>
2024-05-29 14:27:11 +0700

    submit index thcsVanLang

---
+++ TruongThcsVanLang/js/app.js
@@ -0,0 +1,13 @@
+$('.slide-img1').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+  });
+
+  $('.slide-img2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4
+    });
+
