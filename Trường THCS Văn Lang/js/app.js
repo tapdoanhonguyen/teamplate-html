e5b53771cb4a032ce2b83f125c1785bcdee0ecf8
Ho Quy <tanquy039@gmail.com>
2024-05-29 10:40:51 +0700

    Truonghoc

---
+++ Trường THCS Văn Lang/js/app.js
@@ -0,0 +1,14 @@
+$('.slide-img1').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+  });
+
+  $('.slide-img2').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4
+    });
+
+//   $('.slide col-sm-12 .col-md-12').slick();
