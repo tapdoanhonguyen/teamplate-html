4aece64caf948b267fb582439a31e24624cf3b7d
Ho Quy <tanquy039@gmail.com>
2024-06-09 22:01:24 +0700

    evo tour

---
+++ evo-tour/js/app.js
@@ -0,0 +1,9 @@
+// FAQ
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
